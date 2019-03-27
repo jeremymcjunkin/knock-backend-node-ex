07d0fee1a6decacaf8a8bda40d36f995e1a9c21e
Jeremy McJunkin <jeremy@doapps.com>
2019-03-27 10:12:38 -0500

    removed some commented unneeded code

--- load.js
+++ load.js
@@ -23,13 +23,3 @@ function load(inputFile, mls) {
 }
 
 module.exports = load;
-
-// first parameter is the node executable
-// second is the name of this file
-// third is the dataset to load
-// fourth is the mls the data is from
-// we assume we know the mls the data came from
-// if we don't know the source of the mls, we would have to investigate each
-// record and look for know spots for the mls, like date_name and name
-//var properties = load(process.argv[2], process.argv[3]);
-//console.log(properties);
